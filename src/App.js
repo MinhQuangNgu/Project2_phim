ce8e15568891a7a5f05319e428749994632149ff
QuanMinNgu <quangminhnguywn265@gmail.com>
2022-10-06 13:44:10 +0700

    change

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import './App.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import { publicRouter } from './routes/Routes';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <h1>Hello world</h1>
-      </div>
+        <div className="App">
+            <Routes>
+                {publicRouter.map(item => {
+                    const Page = item.element;
+                    return item.exact ? <Route 
+                    element={<Page />}
+                    path={item.path}
+                    exact
+                    /> : 
+                    <Route 
+                    element={<Page />}
+                    path={item.path}
+                    />
+                })}
+            </Routes>
+        </div>
       <ToastContainer />
     </Router>
   );
---
+++ src/routes/Routes.js
@@ -0,0 +1,14 @@
+import Home from "~/components/home/Home"
+
+ 
+export const publicRouter = [
+    {
+        element:Home,
+        path:'/',
+        exact:true
+    }
+]
+
+export const privateRouter = [
+
+]
