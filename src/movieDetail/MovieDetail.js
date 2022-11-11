ea25fc98bd11e6bf38310c9572c008dd270520cb
QuanMinNgu <quangminhnguywn265@gmail.com>
2022-11-11 13:59:40 +0700

    change

--- src/movieDetail/MovieDetail.js
+++ src/movieDetail/MovieDetail.js
@@ -1,5 +1,8 @@
-import React, { useEffect, useRef } from "react";
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import NotFound from "~/notfound/NotFound";
 import "./style.css";
 
 const MovieDetail = () => {
@@ -19,144 +22,155 @@ const MovieDetail = () => {
             });
         }
     };
+
+    const [movie, setMovie] = useState({});
+    const [check, setCheck] = useState(false);
+
+    useEffect(() => {
+        let here = true;
+        let url = `/movie/getone/${slug}`;
+        if (here) {
+            axios
+                .get(url)
+                .then((res) => {
+                    if (!res.data?.movie) {
+                        setCheck(true);
+                    }
+                    setMovie(res.data?.movie);
+                })
+                .catch((err) => {
+                    setCheck(true);
+                    toast.error(err?.response?.data?.msg);
+                });
+        }
+        return () => {
+            here = false;
+        };
+    }, [slug]);
     return (
-        <div className="movie_detail_container">
-            <div className="grid wideS">
-                <div className="movie_detail_wrap">
-                    <div className="movie_infor_container">
-                        <img
-                            className="movie_infor-image"
-                            src="https://congluan-cdn.congluan.vn/files/content/2022/07/06/%E2%80%98minions-su-troi-day-cua-gru%E2%80%99-pha-vo-ky-luc-phong-ve-tai-viet-nam-020737208.jpg"
-                        />
-                        <div className="movie_infor_detail_container">
-                            <div className="movie_infor_detail_image_container">
-                                <img src="https://kenh14cdn.com/203336854389633024/2022/7/5/photo-1-1657007086287434920948.jpg" />
-                            </div>
-                            <div className="movie_infor_detail-clearly">
-                                <div className="movie_infor_detail_title">
-                                    <h1>Cho Kẹo Hay Bị Ghẹo Scooby Doo</h1>
-                                </div>
-                                <div className="movie_infor_detail_enTitle">
-                                    <h2>Cho Kẹo Hay Bị Ghẹo Scooby Doo</h2>
-                                </div>
-                                <div className="movie_infor_detail_button_container">
-                                    <button
-                                        onClick={handleScroll}
-                                        className="movie_infor_detail_button_trailer"
-                                    >
-                                        Trailer
-                                    </button>
-                                    <Link to="/xem-phim/asds">
-                                        <button className="movie_infor_detail_button_watch">
-                                            Xem Phim
-                                        </button>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="movie_infor-more_container">
-                        <div className="movie_infor-more_rating_container">
-                            <div className="movie_infor-more_rating_wrap">
-                                <i className="fa-regular fa-star"></i>
-                                <i className="fa-regular fa-star"></i>
-                                <i className="fa-regular fa-star"></i>
-                                <i className="fa-regular fa-star"></i>
-                                <i className="fa-regular fa-star"></i>
-                                <i className="fa-regular fa-star"></i>
-                                <i className="fa-regular fa-star"></i>
-                                <i className="fa-regular fa-star"></i>
-                                <i className="fa-regular fa-star"></i>
-                                <i className="fa-regular fa-star"></i>
-                                <div className="movie_infor-more-rating_star">
-                                    <i className="fa-solid fa-star"></i>
-                                    <i className="fa-solid fa-star"></i>
-                                    <i className="fa-solid fa-star"></i>
-                                    <i className="fa-solid fa-star"></i>
-                                    <i className="fa-solid fa-star"></i>
-                                    <i className="fa-solid fa-star"></i>
-                                    <i className="fa-solid fa-star"></i>
-                                    <i className="fa-solid fa-star"></i>
-                                    <i className="fa-solid fa-star"></i>
-                                    <i className="fa-solid fa-star"></i>
+        <>
+            {!check ? (
+                <div className="movie_detail_container">
+                    <div className="grid wideS">
+                        <div className="movie_detail_wrap">
+                            <div className="movie_infor_container">
+                                <img
+                                    className="movie_infor-image"
+                                    src="https://congluan-cdn.congluan.vn/files/content/2022/07/06/%E2%80%98minions-su-troi-day-cua-gru%E2%80%99-pha-vo-ky-luc-phong-ve-tai-viet-nam-020737208.jpg"
+                                />
+                                <div className="movie_infor_detail_container">
+                                    <div className="movie_infor_detail_image_container">
+                                        <img src="https://kenh14cdn.com/203336854389633024/2022/7/5/photo-1-1657007086287434920948.jpg" />
+                                    </div>
+                                    <div className="movie_infor_detail-clearly">
+                                        <div className="movie_infor_detail_title">
+                                            <h1>
+                                                Cho Kẹo Hay Bị Ghẹo Scooby Doo
+                                            </h1>
+                                        </div>
+                                        <div className="movie_infor_detail_enTitle">
+                                            <h2>
+                                                Cho Kẹo Hay Bị Ghẹo Scooby Doo
+                                            </h2>
+                                        </div>
+                                        <div className="movie_infor_detail_button_container">
+                                            <button
+                                                onClick={handleScroll}
+                                                className="movie_infor_detail_button_trailer"
+                                            >
+                                                Trailer
+                                            </button>
+                                            <Link to="/xem-phim/asds">
+                                                <button className="movie_infor_detail_button_watch">
+                                                    Xem Phim
+                                                </button>
+                                            </Link>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="movie_infor-more_detail_container">
-                            <ul className="movie_infor-more-detail-wrap">
-                                <li>Trạng thái: HD Vietsub</li>
-                                <li>Thời lượng: 130 phút</li>
-                                <li>Tình trạng: Phim đang chiếu</li>
-                                <li>Ngôn ngữ: Phụ đề Việt</li>
-                                <li>Năm sản xuất: 2022</li>
-                                <li>Quốc gia: Âu - Mỹ</li>
-                                <li>
-                                    Thể loại:
-                                    <Link className="movie_type" to="">
-                                        {" "}
-                                        Phiêu lưu{" "}
-                                    </Link>
-                                    -
-                                    <Link className="movie_type" to="">
-                                        {" "}
-                                        Hành Động{" "}
-                                    </Link>
-                                </li>
-                            </ul>
-                            <div className="movie_content-container">
-                                <h3>Nội dung phim</h3>
-                                <p>
-                                    Phi Công Siêu Đẳng Maverick kể về sau hơn ba
-                                    mươi năm phục vụ, Pete “Maverick” Mitchell
-                                    từng nổi danh là một phi công thử nghiệm quả
-                                    cảm hàng đầu của Hải quân, né tránh cơ hội
-                                    thăng chức, điều khiến anh cảm thấy bị bó
-                                    buộc, để trở về làm chính mình.
-                                </p>
+                            <div className="movie_infor-more_container">
+                                <div className="movie_infor-more_rating_container">
+                                    <div className="movie_infor-more_rating_wrap">
+                                        <i className="fa-regular fa-star"></i>
+                                        <i className="fa-regular fa-star"></i>
+                                        <i className="fa-regular fa-star"></i>
+                                        <i className="fa-regular fa-star"></i>
+                                        <i className="fa-regular fa-star"></i>
+                                        <i className="fa-regular fa-star"></i>
+                                        <i className="fa-regular fa-star"></i>
+                                        <i className="fa-regular fa-star"></i>
+                                        <i className="fa-regular fa-star"></i>
+                                        <i className="fa-regular fa-star"></i>
+                                        <div className="movie_infor-more-rating_star">
+                                            <i className="fa-solid fa-star"></i>
+                                            <i className="fa-solid fa-star"></i>
+                                            <i className="fa-solid fa-star"></i>
+                                            <i className="fa-solid fa-star"></i>
+                                            <i className="fa-solid fa-star"></i>
+                                            <i className="fa-solid fa-star"></i>
+                                            <i className="fa-solid fa-star"></i>
+                                            <i className="fa-solid fa-star"></i>
+                                            <i className="fa-solid fa-star"></i>
+                                            <i className="fa-solid fa-star"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="movie_infor-more_detail_container">
+                                    <ul className="movie_infor-more-detail-wrap">
+                                        <li>Trạng thái: HD Vietsub</li>
+                                        <li>Thời lượng: 130 phút</li>
+                                        <li>Tình trạng: Phim đang chiếu</li>
+                                        <li>Ngôn ngữ: Phụ đề Việt</li>
+                                        <li>Năm sản xuất: 2022</li>
+                                        <li>Quốc gia: Âu - Mỹ</li>
+                                        <li>
+                                            Thể loại:
+                                            <Link className="movie_type" to="">
+                                                {" "}
+                                                Phiêu lưu{" "}
+                                            </Link>
+                                            -
+                                            <Link className="movie_type" to="">
+                                                {" "}
+                                                Hành Động{" "}
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                    <div className="movie_content-container">
+                                        <h3>Nội dung phim</h3>
+                                        <p>
+                                            Phi Công Siêu Đẳng Maverick kể về
+                                            sau hơn ba mươi năm phục vụ, Pete
+                                            “Maverick” Mitchell từng nổi danh là
+                                            một phi công thử nghiệm quả cảm hàng
+                                            đầu của Hải quân, né tránh cơ hội
+                                            thăng chức, điều khiến anh cảm thấy
+                                            bị bó buộc, để trở về làm chính
+                                            mình.
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div ref={trailerRef} className="trailer_container">
-                        <div className="row">
-                            <div className="col c-12 m-0 l-0">
+                            <div ref={trailerRef} className="trailer_container">
                                 <iframe
+                                    className="trailer_container_detail"
                                     width="100%"
-                                    height="200"
                                     src="https://www.youtube.com/embed/giXco2jaZ_4"
                                     title="YouTube video player"
-                                    frameborder="0"
+                                    frameBorder="0"
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                     allowFullScreen
                                 ></iframe>
                             </div>
-                            <div className="col c-0 m-0 l-12">
-                                <iframe
-                                    width="100%"
-                                    height="600"
-                                    src="https://www.youtube.com/embed/giXco2jaZ_4"
-                                    title="YouTube video player"
-                                    frameborder="0"
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                    allowFullScreen
-                                ></iframe>
-                            </div>
-                            <div className="col c-0 m-12 l-0">
-                                <iframe
-                                    width="100%"
-                                    height="400"
-                                    src="https://www.youtube.com/embed/giXco2jaZ_4"
-                                    title="YouTube video player"
-                                    frameborder="0"
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                    allowFullScreen
-                                ></iframe>
-                            </div>
+                            <div className="movie_comment-container"></div>
                         </div>
                     </div>
-                    <div className="movie_comment-container"></div>
                 </div>
-            </div>
-        </div>
+            ) : (
+                <NotFound />
+            )}
+        </>
     );
 };
 
--- src/watchDetail/Watch.js
+++ src/watchDetail/Watch.js
@@ -1,86 +1,146 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import NotFound from "~/notfound/NotFound";
 import "./style.css";
 
 const Watch = () => {
+    const { slug } = useParams();
+
+    const [movie, setMovie] = useState({});
+    const [check, setCheck] = useState(false);
+
+    useEffect(() => {
+        let here = true;
+        let url = `/movie/getone/${slug}`;
+        if (here) {
+            axios
+                .get(url)
+                .then((res) => {
+                    if (!res.data?.movie) {
+                        console.log(res.data?.movie);
+                        setCheck(true);
+                    }
+                    setMovie(res.data?.movie);
+                })
+                .catch((err) => {
+                    setCheck(true);
+                    toast.error(err?.response?.data?.msg);
+                });
+        }
+        return () => {
+            here = false;
+        };
+    }, [slug]);
     return (
-        <div className="watch_container">
-            <div className="grid wideS">
-                <div className="watch_wrap">
-                    <ul className="watch_top_content">
-                        <Link className="watch_top-items" to="/">
-                            <li>Home /</li>
-                        </Link>
-                        <Link className="watch_top-items" to="/">
-                            <li>Hành động /</li>
-                        </Link>
-                        <Link className="watch_top-items" to="/">
-                            <li>Phi Cơ Siêu Đẳng /</li>
-                        </Link>
-                        <Link className="watch_top-items" to="/">
-                            <li>Tập full</li>
-                        </Link>
-                    </ul>
-                    <div className="watcher_container">
-                        <iframe
-                            className="movie_iframe"
-                            width="100%"
-                            src="https://short.ink/6LpgJZOgO"
-                            frameborder="0"
-                            scrolling="0"
-                            allowfullscreen
-                        ></iframe>
-                        <div className="watcher_name_wrap">
-                            <h3>Phi Cơ Siêu Đẳng (Mavel Rick 2022)</h3>
-                        </div>
-                        <div className="episode_container">
-                            <Link className="link_episode" to="/">
-                                <div className="episode_items">Tập 1</div>
-                            </Link>
-                            <Link className="link_episode" to="/">
-                                <div className="episode_items">Tập 1</div>
-                            </Link>
-                            <Link className="link_episode" to="/">
-                                <div className="episode_items">Tập 1</div>
-                            </Link>
-                            <Link className="link_episode" to="/">
-                                <div className="episode_items">Tập 1</div>
-                            </Link>
-                            <Link className="link_episode" to="/">
-                                <div className="episode_items active">
-                                    Tập 1
+        <>
+            {!check ? (
+                <div className="watch_container">
+                    <div className="grid wideS">
+                        <div className="watch_wrap">
+                            <ul className="watch_top_content">
+                                <Link className="watch_top-items" to="/">
+                                    <li>Home /</li>
+                                </Link>
+                                <Link className="watch_top-items" to="/">
+                                    <li>Hành động /</li>
+                                </Link>
+                                <Link className="watch_top-items" to="/">
+                                    <li>Phi Cơ Siêu Đẳng /</li>
+                                </Link>
+                                <Link className="watch_top-items" to="/">
+                                    <li>Tập full</li>
+                                </Link>
+                            </ul>
+                            <div className="watcher_container">
+                                <iframe
+                                    className="movie_iframe"
+                                    width="100%"
+                                    src="https://short.ink/6LpgJZOgO"
+                                    frameBorder="0"
+                                    scrolling="0"
+                                    allowfullscreen
+                                ></iframe>
+                                <div className="watcher_name_wrap">
+                                    <h3>Phi Cơ Siêu Đẳng (Mavel Rick 2022)</h3>
                                 </div>
-                            </Link>
-                            <Link className="link_episode" to="/">
-                                <div className="episode_items">Tập 1</div>
-                            </Link>
-                            <Link className="link_episode" to="/">
-                                <div className="episode_items">Tập 1</div>
-                            </Link>
-                            <Link className="link_episode" to="/">
-                                <div className="episode_items">Tập 1</div>
-                            </Link>
-                            <Link className="link_episode" to="/">
-                                <div className="episode_items">Tập 1</div>
-                            </Link>
-                            <Link className="link_episode" to="/">
-                                <div className="episode_items">Tập 1</div>
-                            </Link>
-                            <Link className="link_episode" to="/">
-                                <div className="episode_items">Tập 1</div>
-                            </Link>
-                            <Link className="link_episode" to="/">
-                                <div className="episode_items">Tập 1</div>
-                            </Link>
-                            <Link className="link_episode" to="/">
-                                <div className="episode_items">Tập 1</div>
-                            </Link>
+                                <div className="episode_container">
+                                    <Link className="link_episode" to="/">
+                                        <div className="episode_items">
+                                            Tập 1
+                                        </div>
+                                    </Link>
+                                    <Link className="link_episode" to="/">
+                                        <div className="episode_items">
+                                            Tập 1
+                                        </div>
+                                    </Link>
+                                    <Link className="link_episode" to="/">
+                                        <div className="episode_items">
+                                            Tập 1
+                                        </div>
+                                    </Link>
+                                    <Link className="link_episode" to="/">
+                                        <div className="episode_items">
+                                            Tập 1
+                                        </div>
+                                    </Link>
+                                    <Link className="link_episode" to="/">
+                                        <div className="episode_items active">
+                                            Tập 1
+                                        </div>
+                                    </Link>
+                                    <Link className="link_episode" to="/">
+                                        <div className="episode_items">
+                                            Tập 1
+                                        </div>
+                                    </Link>
+                                    <Link className="link_episode" to="/">
+                                        <div className="episode_items">
+                                            Tập 1
+                                        </div>
+                                    </Link>
+                                    <Link className="link_episode" to="/">
+                                        <div className="episode_items">
+                                            Tập 1
+                                        </div>
+                                    </Link>
+                                    <Link className="link_episode" to="/">
+                                        <div className="episode_items">
+                                            Tập 1
+                                        </div>
+                                    </Link>
+                                    <Link className="link_episode" to="/">
+                                        <div className="episode_items">
+                                            Tập 1
+                                        </div>
+                                    </Link>
+                                    <Link className="link_episode" to="/">
+                                        <div className="episode_items">
+                                            Tập 1
+                                        </div>
+                                    </Link>
+                                    <Link className="link_episode" to="/">
+                                        <div className="episode_items">
+                                            Tập 1
+                                        </div>
+                                    </Link>
+                                    <Link className="link_episode" to="/">
+                                        <div className="episode_items">
+                                            Tập 1
+                                        </div>
+                                    </Link>
+                                </div>
+                            </div>
+                            <div className="comment_container"></div>
                         </div>
                     </div>
-                    <div className="comment_container"></div>
                 </div>
-            </div>
-        </div>
+            ) : (
+                <NotFound />
+            )}
+        </>
     );
 };
 
