56274e50fc75299bc0e3efec45f94d51259e8fd4
MinhQuangNgu <quangnm160393@gmail.com>
2022-12-11 19:04:12 +0700

    change

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(
     cors({
         credentials: true,
-        origin: "https://sttruyen.xyz",
+        origin: ["https://sttruyen.xyz", "http://localhost:3000"],
     })
 );
 
